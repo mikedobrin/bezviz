acd9ed20014e7c832ba1805bf051c5c7e2aba7a9
Mike Dobrin <inter4_44094@yahoo.com>
2016-08-02 22:13:40 +0200

    removed comments

--- scripts/timeline.js
+++ scripts/timeline.js
@@ -1,28 +1,5 @@
 $(function () {
 
-// 15/07/2009	Legislative proposal published	COM(2009)0366 Eur-Lex link	Summary
-// 14/09/2009	Committee referral announced in Parliament, 1st reading/single reading
-// 19/10/2009	Vote in committee, 1st reading/single reading	 	Summary
-// 27/10/2009	Committee report tabled for plenary, 1st reading/single reading	A7-0042/2009
-// 11/11/2009	Debate in Parliament
-// 12/11/2009	Results of vote in Parliament
-// 12/11/2009	Decision by Parliament, 1st reading/single reading	T7-0062/2009	Summary
-// 30/11/2009	Act adopted by Council after consultation of Parliament
-// 30/11/2009	End of procedure in Parliament
-// 18/12/2009	Final act published in Official Journal
-
-// 27/11/2013	Legislative proposal published	COM(2013)0853 Eur-Lex link	Summary
-// 13/01/2014	Committee referral announced in Parliament, 1st reading/single reading
-// 12/02/2014	Vote in committee, 1st reading/single reading
-// 13/02/2014	Committee report tabled for plenary, 1st reading/single reading	A7-0104/2014	Summary
-// 27/02/2014	Results of vote in Parliament
-// 27/02/2014	Debate in Parliament
-// 27/02/2014	Decision by Parliament, 1st reading/single reading	T7-0166/2014	Summary
-// 14/03/2014	Act adopted by Council after Parliament's 1st reading
-// 03/04/2014	Final act signed
-// 03/04/2014	End of procedure in Parliament
-// 08/04/2014	Final act published in Official Journal
-
 	var stages = ["Профільний комітет ЄП обирає доповідача",
 		"Узгодження законопроекту в комітеті ЄП",
 		"Проект готується до подання на розгляд в Європарламенті",
